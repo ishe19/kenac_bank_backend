eb5ef37da1a8c09c5c8ce4bf9bb1c50bc00c6e70
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2025-07-11 09:07:15 +0200

    Create BankClient.java

---
+++ services/client-service/src/main/java/com/kenacbank/clientservice/services/interfaces/BankClient.java
@@ -0,0 +1,16 @@
+package com.kenacbank.clientservice.services.interfaces;
+
+import com.kenacbank.clientservice.models.requests.OpenAccountRequest;
+import com.kenacbank.clientservice.models.responses.GenericResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+
+@FeignClient(name = "bank-service", url = "${application.config.banking-url}")
+public interface BankClient {
+
+    @PostMapping("/banking/open-account")
+    Optional<GenericResponse> openClientAccount(@RequestBody OpenAccountRequest request);
+}
